6844cfad4008f20884a327d480008adca30085b1
Patricija Delac <patricija.delac@agiledrop.com>
2025-01-03 11:04:41 +0100

    Add vite-static-copy plugin and update image paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // Path to the global SCSS file
 const scssPath = path.resolve(__dirname, './src/styles/lib.scss');
@@ -18,6 +19,16 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/images/*', // Copies all images
+          dest: 'assets/images', // Places them in the 'dist/assets/images' directory
+        },
+      ],
+    }),
+  ],
   base: '/results-summary-component/',
 });
